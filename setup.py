c57aefed982053aa9929af1fc22860dbc0408eaf
Raul Mangolin <eu@raulmangolin.com>
2020-06-17 15:50:38 -0300

    version fix

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='mangosort',
-    version='0.1.1',
+    version='0.2.0',
     url='https://github.com/raulmangolin/mangosort',
     license='MIT License',
     author='Raul Mangolin',
