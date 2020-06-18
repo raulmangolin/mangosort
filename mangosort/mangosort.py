3dddbc38d5e02c036d3b669fdd942f17ed5db9ce
Raul Mangolin <eu@raulmangolin.com>
2020-06-18 12:59:40 -0300

    now you can pass asc/desc or True/False

--- mangosort/mangosort.py
+++ mangosort/mangosort.py
@@ -6,16 +6,18 @@ def sort_by_key(my_list=[], keys=[]):
         :param (list) my_list: list of objects you want to order
             [{'code': 'beta', 'number': 3}, {'code': 'delta', 'number': 2}]
 
-        :param (list) keys: list of keys and direction to order.
-            eg: [{'code': True}, {'number': False}]
+        :param (list) keys: list of dict keys and direction to order.
+            eg: [{'code': 'desc'}, {'number': 'asc'}]
 
     Returns:
         :return (list) my_list: your list reordered
     """
 
     if keys:
-        first = keys[0]
-        remaining = keys[1:]
+        data_keys = parse_keys(keys)
+
+        first = data_keys[0]
+        remaining = data_keys[1:]
 
         current_key = list(first.keys())[0]
         first_value = list(first.values())[0]
@@ -25,6 +27,25 @@ def sort_by_key(my_list=[], keys=[]):
         return sorted(presorted_list, key=lambda row: row[current_key], reverse=first_value)
     else:
         return my_list
+
+
+def parse_keys(keys):
+    data = []
+
+    if type(keys) == list:
+        for i in keys:
+            for e in i:
+                if type(i[e]) == bool:
+                    value = i[e]
+                elif type(i[e]) == int:
+                    value = True if i[e] > 0 else False
+                else:
+                    value = True if i[e].lower() == 'desc' else False
+                data.append({e: value})
+    else:
+        data = keys
+
+    return data
 
 
 def sort_by_key_desc(my_list=[], keys=[]):
